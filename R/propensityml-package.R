6554f5e5ebb14d9fe310dcbce208291d02a1643f
Young-geun Kim <dudrmshub@gmail.com>
2020-11-06 16:00:19 +0900

    package description

---
+++ R/propensityml-package.R
@@ -0,0 +1,12 @@
+#' @aliases propensityml propensityml-package
+#' @details
+#' The propensityml package provides various ML functions to estimate propensity score.
+#' @references Lee, B. K., Lessler, J., & Stuart, E. A. (2010). \emph{Improving propensity score weighting using machine learning. Statistics in Medicine}. Statistics in Medicine, 29(3), 337-346.
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
